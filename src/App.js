63fc0f46b24430e5e127c28cfb68f9cbee9ed68d
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-04-09 20:32:03 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import NavigationBar from './components/navigation/NavigationBar';
+import Banner from './components/banner/Banner';
+import Layout from './components/layout/Layout';
+import MessageListContainer from './components/messages/MessageListContainer';
+import SubHeader from './components/subHeader/SubHeader';
+import './App.css';
+
+const book = require('./images/book-iso.png');
+const park = require('./images/tree-iso.png');
+const cannedGood = require('./images/can-iso.png');
+
+class App extends Component {
+    render() {
+        return (<
+            Layout>
+            <NavigationBar/> {
+                /* <svg id="waves" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 222.73">
+                    <title>waves</title>
+                    <path className="cls-1" d="M0,109S315,213,553,213,1191-21,1600,48V258H0Z" transform="translate(0 -35.27)" />
+                  </svg> */
+            } <Banner/>
+            <div className = "main" >
+            <div className = "squeeze" >
+            <div className = "sub-container-flex" >
+            <SubHeader padding = "extra-padding"
+            title = "Investing"
+            subContent = { cannedGood }
+            animation = "reg" / >
+            <SubHeader title = "Finance"
+            subContent = { book }
+            animation = "medium" / >
+            <SubHeader title = "NFTs"
+            subContent = { park }
+            animation = "slow" / >
+            </div> <div className = "message-section-title" > Cutting out the banks from the Lending Systems interface. </div> <div className = "message-flex">
+            <MessageListContainer / > { /* <div className='round-circle'></div> */ } </div> </div > </div> </Layout>
+        );
+    }
+}
+
+export default App;
