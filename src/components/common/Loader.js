e613eb6a03c802ab0aed2377b110f4913cf531e5
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-04-09 20:46:39 +0300

    Add files via upload

---
+++ src/components/common/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+ const Loader = () => {
+    return (
+      <div className='ring'>
+        <span></span>
+      </div>
+    )
+}
+
+export default Loader;
