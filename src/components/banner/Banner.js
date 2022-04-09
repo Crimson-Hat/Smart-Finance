5cfea841ad58b2bb720659c6e402b2802ffe80a5
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-04-09 20:44:55 +0300

    Create Banner.js

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import DonateForm from '../form/DonateForm';
+import iphoneX from '../../images/Asset2@2x.png';
+import charity from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+import './Banner.css';
+import '../../App.css';
+
+export default class Banner extends Component {
+    state = {
+        modalOpen: false,
+        balance: '',
+        donatorsCount: '',
+        charityCount: '',
+    }
+    componentDidMount = async event => {
+        const summary = await charity.methods.getSummary().call();
+        this.setState({
+                balance: summary[0],
+                donatorsCount: summary[1],
+                charityCount: summary[2]
+            })
+            // console.log(summary);
+    }
+    clickOpenModal = () => {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
+    }
+
+    render() {
+        // const { balance, donatorsCount, charityCount} = this.props
+        return (<div>
+            <div className = 'banner-background' >
+            <DonateForm clickOpen = { this.state.modalOpen }
+            onClose = { this.clickOpenModal }
+            /> <div className = 'main-banner' >
+            <div className = 'squeeze' >
+            <div className = 'banner-flex' >
+            <div className = 'column__one-third'> {/*<div className='banner-caption'>Join the cause and help others in need</div> */
+            } <div className = 'banner-header' > Cutting out the bankers from the Banking Lending System. < /div> <
+            div className = 'banner-caption__description-amount' > Our goal is to improve reduce loans interest an boost lenders profits. { this.state.charityCount }! < /div > <
+            button className = 'donate-button__banner'onClick = {() => this.clickOpenModal()}> Invest </button>
+	<button className = 'info-button__banner' > More Info < /button> </div >
+ <div className = 'column__two-third' ><img className = 'iphonex-asset' src = { iphoneX } alt = "iPhoneX"/>
+            </div> </div></div> </div > </div> </div >
+        )
+    }
+}
