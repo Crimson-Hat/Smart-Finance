bb7ac21d1921b2be65eab0c4ed8de8a08494083f
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-04-09 20:32:33 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,29 +12,45 @@ const cannedGood = require('./images/can-iso.png');
 
 class App extends Component {
     render() {
-        return (<
-            Layout>
-            <NavigationBar/> {
+        return ( <
+            Layout >
+            <
+            NavigationBar / > {
                 /* <svg id="waves" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 222.73">
                     <title>waves</title>
                     <path className="cls-1" d="M0,109S315,213,553,213,1191-21,1600,48V258H0Z" transform="translate(0 -35.27)" />
                   </svg> */
-            } <Banner/>
-            <div className = "main" >
-            <div className = "squeeze" >
-            <div className = "sub-container-flex" >
-            <SubHeader padding = "extra-padding"
-            title = "Investing"
-            subContent = { cannedGood }
+            } <
+            Banner / >
+            <
+            div className = "main" >
+            <
+            div className = "squeeze" >
+            <
+            div className = "sub-container-flex" >
+            <
+            SubHeader padding = "extra-padding"
+            title = "Food"
+            subContent = { Investing }
             animation = "reg" / >
-            <SubHeader title = "Finance"
-            subContent = { book }
+            <
+            SubHeader title = "Education"
+            subContent = { Finance }
             animation = "medium" / >
-            <SubHeader title = "NFTs"
-            subContent = { park }
+            <
+            SubHeader title = "Recreation"
+            subContent = { NFTS }
             animation = "slow" / >
-            </div> <div className = "message-section-title" > Cutting out the banks from the Lending Systems interface. </div> <div className = "message-flex">
-            <MessageListContainer / > { /* <div className='round-circle'></div> */ } </div> </div > </div> </Layout>
+            <
+            /div> <
+            div className = "message-section-title" > Cutting out the banks from the Lending Systems < /div> <
+            div className = "message-flex" >
+            <
+            MessageListContainer / > { /* <div className='round-circle'></div> */ } <
+            /div> < /
+            div > <
+            /div> < /
+            Layout >
         );
     }
 }
